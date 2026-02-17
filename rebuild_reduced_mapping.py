5b5e923698891a6aa8d3f84b7cb7ecac47f46bcb
Alexandre Daby <alexandre.daby-seesaram@ensta-paris.fr>
2026-02-17 19:07:15 +0100

    WIP

--- rebuild_reduced_mapping.py
+++ rebuild_reduced_mapping.py
@@ -3,4 +3,10 @@ import dolfin_warp as dwarp
 import os
 import glob
 
-
+def save_rebuilt_mapping(
+        mesh                                                                   ,
+        lung                        = "RL"                                     , 
+        basename                    = "Barycenter_fine_mapping"                , 
+        mappings_basename           = "Mapping_fine_sphere"                    , 
+        regul_model                 = "ogdenciarletgeymonatneohookean"         ,
+         )
